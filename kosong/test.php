e4cae2058590b9979c41584f2cbe0e9e0b655cc1
SYAFI MA'ARIF R.H <109969103+Syafi120@users.noreply.github.com>
2023-08-04 14:01:07 +0700

    memperbaiki kesalahan pada home.php

--- kosong/test.php
+++ kosong/test.php
@@ -1,51 +1,29 @@
 <?php
+
     session_start();
+    
     include "../function.php";
     
-    if (isset($_POST['email']) && isset($_POST['pass'])) {
+    $email = $_POST["email"];
+    $pass = $_POST["pass"];
+    
+    $sql = "SELECT * FROM tbllogin WHERE email='" . $email . "' and pass='".$pass."' limit 1";
+    $hasil = mysqli_query($koneksi,$sql);
+    $jumlah = mysqli_num_rows($hasil);
+    // var_dump($hasil);
+
+    die();
+    if ($jumlah>0) {
+        $row = mysqli_fetch_assoc($hasil);
         
-        function validate($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
+        $_SESSION["idLogin"]=$row["idLogin"];
+        $_SESSION["email"]=$row["email"];
+        $_SESSION["username"]=$row["username"];
         
-        $email = validate($_POST['email']);
-        $pass = validate($_POST['pass']);
-
-        $koneksi = new mysqli($email, $pass);
-        if ($koneksi->connect_error) {
-            die("Failed : " . $koneksi->connect_error);
-        }else {
-            $sql = "SELECT * FROM tbllogin WHERE email='$email' AND pass ='$pass'";
-            
-            $result = $koneksi->query($sql);
-            var_dump($result);
-            die('haloo');
-
-            if (mysqli_num_rows($result) === 1 && "POST") {
-                die('haloo');
-                $row = mysqli_fetch_assoc($result);
-
-                if ($row['email'] === $email && $row ['pass'] === $pass ) {
-                    $_SESSION['email'] = $email;
-                    $_SESSION['pass'] = $pass;
-                    header("Location: home.php");
-                    exit();
-                }else {
-                    header("Location: index.php?error=Incoret_email_or_pass");
-                    exit();
-                }
-
-            }else {
-                header("Location index.php?error=incorect_email_or_pass");
-                exit();
-            }
-        }
-        
+        die(header("Location : home.php"));
     }else {
-        header("Location: index.php");
-        exit();
+        echo "username atau password salah <br>
+        <a href=index.php>Kembali</a>";
     }
+
 ?>
